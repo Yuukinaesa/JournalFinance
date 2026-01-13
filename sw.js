f8df52b05dedf376f0c2ab71791b68d8c7302faf
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2026-01-14 02:49:48 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'journal-finance-v1';
+const ASSETS_TO_CACHE = [
+    './JournalFinance.html',
+    './manifest.json',
+    './icon.png'
+];
+
+// Install Event - Cache Files
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event - Clean old caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Fetch Event - Serve from Cache or Network
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
